0b1afc630f1da686600701c97dace021f60f220e
Vladimir <vladimirg@moonsite.co.il>
2023-01-11 16:33:39 +0200

    clean proj

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
-import { EventList } from '../components/events/EventList';
-import { getFeaturedEvents } from '../dummy-data';
-
 const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
   return (
     <div>
-      <EventList items={featuredEvents} />
     </div>
   );
 };
